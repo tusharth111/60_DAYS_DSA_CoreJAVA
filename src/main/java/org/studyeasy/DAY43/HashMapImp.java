636ff59908d7d3a349ae2fca33d6d2e310804dbd
Tushar Thakur <tusharth111@gamil.com>
2026-05-22 10:42:14 +0530

    HAD last sem exam  now again start - HashMap THEORY AND IMPLEMENTATION

---
+++ src/main/java/org/studyeasy/DAY43/HashMapImp.java
@@ -0,0 +1,21 @@
+package org.studyeasy.DAY43;
+import java.util.HashMap;
+/***
+ Developer Name : Tushar Thakur
+ Developer Contact : tusharth111@gmail.com
+ Created on:  21 5/21/2026 12:21 PM
+ Project Name : 30Days_Java
+ ***/
+//HashMap implementation and interview thought process and LEETCODE #1 Two Sum
+public class HashMapImp {
+    public static void main(String[] args) {
+        HashMap<Integer,String> hm = new HashMap<>();// IMPLEMENTATION OF THE HASHMAP
+        //Adding
+        hm.put(1,"Tushar");
+        hm.put(2,"Aryan");
+        hm.put(3,"Rohan");
+        //Accessing
+        System.out.println(hm.get(1));
+
+    }
+}
